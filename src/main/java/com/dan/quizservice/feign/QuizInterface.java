231c38bc22073bbe61bf5da4accb1fd0483566f3
Dan Cross <Dancross96@gmail.com>
2023-08-03 10:09:32 -0500

    added proper method declarations in the feign interface

--- src/main/java/com/dan/quizservice/feign/QuizInterface.java
+++ src/main/java/com/dan/quizservice/feign/QuizInterface.java
@@ -1,4 +1,25 @@
 package com.dan.quizservice.feign;
 
+import com.dan.quizservice.model.QuestionWrapper;
+import com.dan.quizservice.model.Response;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@FeignClient("QUESTION-SERVICE")
 public interface QuizInterface {
+    @GetMapping("generate")
+    public ResponseEntity<List<Integer>> getQuestionsFromQuiz(@RequestParam String categoryName, @RequestParam Integer numQuestions);
+
+    @PostMapping("getQuestions")
+    public ResponseEntity<List<QuestionWrapper>> getQuestionsFromId(@RequestBody List<Integer> questionIds);
+
+
+    @PostMapping("getScore")
+    public ResponseEntity<Integer> getScore(@RequestBody List<Response> responses);
 }
